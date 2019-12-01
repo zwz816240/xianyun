dff4ef9e96d91d76448f7d8766020ac9d7d3ec11
zwz816240 <55817717+zwz816240@users.noreply.github.com>
2019-12-01 21:21:17 +0800

    04实现store用户信息托管

--- components/user/loginForm.vue
+++ components/user/loginForm.vue
@@ -47,24 +47,54 @@ export default {
     }
   },
   methods: {
+    //     handleLoginSubmit () {
+    //       // 验证表单
+    //       this.$refs.form.validate(async (valid) => {
+    //         // 验证表单通过时发送请求
+    //         if (valid) {
+    //           const res = await this.$axios({
+    //             url: '/accounts/login',
+    //             method: 'POST',
+    //             data: this.form
+    //           })
+    //           // eslint-disable-next-line no-console
+    //           console.log(res.data)
+    //         } else {
+    //           // 验证不通过
+    //           // 给提示
+    //           this.$message.error('用户数据输入不合法')
+    //           // 中止本次请求
+    //           return false
+    //         }
+    //       })
+    //     }
+    // 提交登录
     handleLoginSubmit () {
       // 验证表单
-      this.$refs.form.validate(async (valid) => {
-        // 验证表单通过时发送请求
+      this.$refs.form.validate((valid) => {
+        // 为true表示没有错误
         if (valid) {
-          const res = await this.$axios({
-            url: '/accounts/login',
-            method: 'POST',
-            data: this.form
+          // 删除代码
+          // this.$axios({
+          //     url: "/accounts/login",
+          //     method: "POST",
+          //     data: this.form
+          // }).then(res => {
+          //     console.log(res.data);
+          // })
+
+          // 新增代码
+          this.$store.dispatch('user/login', this.form).then((res) => {
+            // 成功提示
+            this.$message({
+              message: '登录成功，正在跳转',
+              type: 'success'
+            })
+            // 跳转到首页
+            setTimeout(() => {
+              this.$router.replace('/')
+            }, 1000)
           })
-          // eslint-disable-next-line no-console
-          console.log(res.data)
-        } else {
-          // 验证不通过
-          // 给提示
-          this.$message.error('用户数据输入不合法')
-          // 中止本次请求
-          return false
         }
       })
     }
--- store/user.js
+++ store/user.js
@@ -6,3 +6,26 @@ export const state = () => ({
     user: {}
   }
 })
+
+export const mutations = {
+  // 保存用户信息到state
+  setUserInfo (state, data) {
+    state.userInfo = data
+  }
+}
+
+export const actions = {
+  // 登录
+  login ({ commit }, data) {
+    return this.$axios({
+      url: '/accounts/login',
+      method: 'POST',
+      data
+    }).then((res) => {
+      const data = res.data
+      // 保存到state
+      commit('setUserInfo', data)
+      return data
+    })
+  }
+}
