b0a4fdd3bd11226929cf08df192cb4b2b9e272ee
zwz816240 <55817717+zwz816240@users.noreply.github.com>
2019-12-07 19:04:42 +0800

    10获取特价机票数据渲染

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -38,7 +38,20 @@
     </h2>
 
     <!-- 特价机票 -->
-    <div class="air-sale" />
+    <!-- 特价机票 -->
+    <div class="air-sale">
+      <el-row type="flex" class="air-sale-pic" justify="space-between">
+        <el-col :span="6" v-for="(item, index) in sales" :key="index">
+          <nuxt-link :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`">
+            <img :src="item.cover">
+            <el-row class="layer-bar" type="flex" justify="space-between">
+              <span>{{ item.departCity }}-{{ item.destCity }}</span>
+              <span>￥699</span>
+            </el-row>
+          </nuxt-link>
+        </el-col>
+      </el-row>
+    </div>
   </section>
 </template>
 
@@ -48,6 +61,19 @@ import SearchForm from '@/components/air/searchForm'
 export default {
   components: {
     SearchForm
+  },
+  data () {
+    return {
+      sales: []
+    }
+  },
+  mounted () {
+    // 获取特价机票并赋值渲染
+    this.$axios({
+      url: '/airs/sale'
+    }).then((res) => {
+      this.sales = res.data.data
+    })
   }
 }
 </script>
